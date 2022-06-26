6c353283466127e1b47a3b0703b8752e621644c2
Sophia Davis <sophiadavis@Sophias-MBP.attlocal.net>
2022-06-26 19:14:25 -0400

    reformatted app.js to match the new folders

--- developer/src/App.js
+++ developer/src/App.js
@@ -1,42 +1,35 @@
 import React, { useState } from 'react';
-import Nav from './components/Nav';
 import About from './components/About';
-import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
+import Footer from './components/Footer';
+import Portfolio from './components/Portfolio';
+import Nav from './components/Nav';
+
+function router (page) {
+  switch (page) {
+      case "contact":
+        return <ContactForm />
+
+      case "portfolio":
+        return <Portfolio />
+
+      default:
+          return <About />
+  }
+}
 
 function App() {
-  const [categories] = useState([
-    {
-      name: 'commercial',
-      description: 'Photos of grocery stores, food trucks, and other commercial projects',
-    },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-  ]);
-
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
-  const [contactSelected, setContactSelected] = useState(false);
+  const [selectedPage, setPage] = useState('home');
 
   return (
     <div>
-      <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
-        contactSelected={contactSelected}
-        setContactSelected={setContactSelected}
-      ></Nav>
+      <Nav seletected={selectedPage} onSelect={setPage} />
       <main>
-        {!contactSelected ? (
-          <>
-            <Gallery currentCategory={currentCategory}></Gallery>
-            <About></About>
-          </>
-        ) : (
-          <ContactForm></ContactForm>
-        )}
+   
+
+        { router(selectedPage) }
+        
+        <Footer />
       </main>
     </div>
   );
